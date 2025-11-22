f5489daa2c3163f3e68a1e75935232131edd4784
jamarten291 <146338884+jamarten291@users.noreply.github.com>
2025-11-22 14:53:21 +0100

    add SimuladorServidor main

--- src/main/java/simulador_pool/SimuladorServidor.java
+++ src/main/java/simulador_pool/SimuladorServidor.java
@@ -14,25 +14,22 @@ class PoolConexiones {
         return new PoolConexiones();
     }
 
+    private int conexionesDisponibles() {
+        return MAX_CONEXIONES - conexiones;
+    }
+
     public synchronized void conectar(String thread) throws InterruptedException {
         while (conexiones >= MAX_CONEXIONES) {
             System.out.printf("[%s] esperando conexión...\n", thread);
             wait();
         }
         conexiones++;
-        System.out.printf("[%s] obtuvo conexión. Disponibles: %d \n", thread, this.conexiones);
+        System.out.printf("[%s] obtuvo conexión. Disponibles: %d \n", thread, this.conexionesDisponibles());
     }
 
     public synchronized void desconectar(String thread) {
-//        while (conexiones < 0) {
-//            try {
-//                wait();
-//            } catch (InterruptedException ex) {
-//                Logger.getLogger(PoolConexiones.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//        }
         conexiones--;
-        System.out.printf("[%s] liberó conexión. Disponibles: %d \n", thread, this.conexiones);
+        System.out.printf("[%s] liberó conexión. Disponibles: %d \n", thread, this.conexionesDisponibles());
         notifyAll();
     }
 }
@@ -44,11 +41,11 @@ class TareaBBDD extends Thread {
     final PoolConexiones pool;
     final Random r;
 
-    public TareaBBDD(String nombre) {
+    public TareaBBDD(String nombre, PoolConexiones pool) {
         super(nombre);
         this.nombre = nombre;
         this.id = contId++;
-        this.pool = PoolConexiones.getInstance();
+        this.pool = pool;
         this.r = new Random();
     }
 
@@ -76,15 +73,56 @@ class TareaBBDD extends Thread {
             } finally {
                 pool.desconectar(this.nombre);
 
-                // Informa de que ha terminado dependiendo del code
-                System.out.printf((ok ? "[%s] terminó correctamente.\n" :
-                        "[%s] terminó sin completar la query"),
-                        this.nombre
-                );
+                // Informa que ha terminado dependiendo del resultado de la consulta
+                System.out.printf((ok ?
+                        "[%s] terminó correctamente.\n" :
+                        "[%s] terminó sin completar la query.\n"),
+                        this.nombre);
             }
         }
     }
 }
 
+class AdminBBDD implements Runnable {
+    TareaBBDD[] listaTareas;
+    Random r;
+
+    AdminBBDD(TareaBBDD[] listaTareas) {
+        this.listaTareas = listaTareas;
+        this.r = new Random();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            int segundos = r.nextInt(1000,2001);
+            try {
+                Thread.sleep(segundos);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            int index = r.nextInt(listaTareas.length);
+            TareaBBDD tareaInterrumpida = listaTareas[index];
+            tareaInterrumpida.interrupt();
+        }
+    }
+}
+
 public class SimuladorServidor {
+    public static void main(String[] args) {
+        TareaBBDD[] tareas = new TareaBBDD[10];
+        PoolConexiones pool = PoolConexiones.getInstance();
+
+        for (int i = 0; i < tareas.length; i++) {
+            tareas[i] = new TareaBBDD("Tarea-" + i, pool);
+        }
+
+        Thread admin = new Thread(new AdminBBDD(tareas));
+
+        for (TareaBBDD t : tareas) {
+            t.start();
+        }
+
+        admin.start();
+    }
 }
